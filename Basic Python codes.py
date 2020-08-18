4aed8efc5f82a31839aef2bb01a347b050338ff5
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-08-18 15:13:36 +0530

    Basic python codes added

---
+++ Basic Python codes.py
@@ -0,0 +1,51 @@
+1] print("Hello world")
+   #This program prints Hello world.
+   
+  OUTPUT:
+   Hello world
+
+2] a=1
+   b=2
+   print(a+b)
+   #This program adds two numbers.
+   
+  OUTPUT:
+   3
+   
+3] Program  to find the Simple Interest.
+   #different inputs
+     P=1
+     R=1
+     T=1
+   #calculate the simple interest.
+     SI=P*R*T/100
+   #print the resultant value of SI.
+     print("Simple Interest=",SI)
+     
+   OUTPUT:
+    Simple Interest=0.01
+    
+ 4] What is the output of this code.
+       7!=8
+       print(7!=8)
+       
+     OUTPUT:
+      True
+      
+  5] What is the output of the following code?
+          x=7
+          print(x)
+          print(x+3)
+          print(x)
+          
+      OUTPUT:
+        7
+        10
+        7
+        
+  6] What is the output of this code?
+        a=" eggs"
+        print(a*3)
+        
+      OUTPUT:
+        eggs eggs eggs

6d83035257fdb51b67a5c270f9efa49ce74c297b
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-08-19 14:41:57 +0530

    Python indexing is created

---
+++ Python Indexing.py
@@ -0,0 +1,25 @@
+INDEXING IN PYTHON..
+**Every word inn this string has a unique index i,e position of the word in the particular value.
+**In the string the number will begin from zero.
+
+EXAMPLES:-
+1]  x="Water"
+   #If we want 't' somewhere then,
+    x[2]
+    
+  OUTPUT:
+   't'
+   
+2] x="Water"   
+   x[3]
+   
+  OUTPUT:
+    'e'  
+    
+ 3] x="Hello world"
+    x=[6]
+    
+  OUTPUT:
+    'w'
+    
+    ****So this is known as INDEX IN PYTHON *****

8d5fafb896cdd7a385d25f46a16b0ec178f602ca
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-08-19 11:24:53 +0530

    Methods created

---
+++ Methods and Strings.py
@@ -0,0 +1,33 @@
+#If we need  to know the methods to string,then write
+---> dir("string")
+Examples for Methods:-
+ => x="water"
+    x.title()
+   #To get w Capital.
+   
+  OUTPUT:
+     'Water'
+     
+ => x="WATER"
+    x.lower()
+   #To get lower case letters.
+  
+  OUTPUT:
+   'water'
+   
+ => If we want to replace any word  then.
+      
+      x.replace()
+      
+   ex:-
+       x="Hello world"
+       x.replace('l','a')
+       
+     OUTPUT:
+       Heaao worad
+
+
+
+
+
+

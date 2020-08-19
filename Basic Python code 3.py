47729c77cbb54c826a1e8b7fbf34e78886b1a46f
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-08-19 10:57:26 +0530

    basic python code 3 created

---
+++ Basic Python code 3.py
@@ -0,0 +1,35 @@
+10] Write a python script to take 2 names from the user and join them.
+        print("enter the 2 names:")
+        a=input()
+        b=input()
+        print(a+b)
+    OUTPUT:
+     enter the 2 names:
+        hello 
+        world
+        hello world
+        
+                         [or]
+                         
+        print("enter the 2 names:")
+        a=input()
+        b=input()
+        print("Result=",c)
+      OUTPUT:
+         enter the 2 names:
+         hello
+         world
+         Result=helloworld
+         
+                      [or]
+                      
+        print("enter the 2 names:")
+        a=input()
+        b=input()
+        print("Result=",a+b)
+      OUTPUT:
+        enter the 2 names:
+        hello 
+        world
+        Result=helloworld
+      

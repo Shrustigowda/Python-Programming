27bda3233b94c3179b1527593e6d1246eeb80c82
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-08-19 15:14:01 +0530

    slicing in python is created

---
+++ Slicing in Python.py
@@ -0,0 +1,36 @@
+EXAMPLES:-
+1] x="hello world"
+   x[1:4]   ---------->#from e upto o
+   
+  OUTPUT:
+    'ell'
+
+2] x="hello world"
+   x[1:5]
+   
+  OUTPUT:
+    'ello'
+3] #If we want to access value from negative index ,then
+   x="hello world"
+   x[-2]
+   
+ OUTPUT:
+    'l'
+
+4] x="hello world"
+   x[-3:-1]
+   
+ OUTPUT:
+    'rl'
+    
+5] x="hello world"
+   x[0:6]
+   
+  OUTPUT:
+     'hello'
+  #Shortcut for this is ..
+   x[:6]
+   
+ OUTPUT:
+  'hello'
+    

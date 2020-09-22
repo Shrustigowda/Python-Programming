88c63f9c66eb44b76302986c68f3ba3000a89efe
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-09-22 18:46:10 +0530

    Tuples created

---
+++ Tuples in Python .py
@@ -0,0 +1,16 @@
+#Unique charecteristics are:-
+* Tuples are in franics ->(),like  x=().
+* Tuples elements cannot be changed or cannot be manupilated.
+Examples:-
+
+1] x=("red","blue","green","yellow","black",12,4.3)
+   x.append(34)
+   
+#It will be an error bcoz tuples elements cannot be changed.
+#If we need to know the methods in tuples then use 
+   dir(tuples)
+#The list of methods in the tuples will be displayed.
+*Once the tuple is created it will be same towards the program.
+*Tuples are used less due to their less flexibility.
+*But tuples are used in some typical cases.
+*But adding and removing from tuples is not possible.
